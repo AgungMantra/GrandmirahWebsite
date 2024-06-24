98b77c7410db50d815ff56dcd32689556621bdcd
AgungMantra <agung.mantra07@gmail.com>
2024-06-24 12:43:25 +0800

    update gallery & home

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ ScrollReveal().reveal('#Srscale,.Srscale', { scale: 0.85 });
 ScrollReveal().reveal('#Srleft' ,{ origin: 'left' });
 ScrollReveal().reveal('#Srright' ,{ origin: 'right' });
 ScrollReveal().reveal('#Srbottom' ,{ origin: 'bottom' });
+
+// Typewritter effect
+const words = ["Welcome To", "Grand Mirah Hotel"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 200);
+}
+
+type();
+
